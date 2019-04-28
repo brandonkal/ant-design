19eb8eef9e504a3d82998ec494ed6c8f9f2d151b
Brandon Kalinowski <brandon@brandonkalinowski.com>
2019-04-27 21:38:36 -0700

    [WIP] colors presets

---
+++ components/style/color/colors.less.js
@@ -0,0 +1,162 @@
+import 'colorPalette';
+
+// color palettes
+const blue1 = colorPalette(blue6, 1);
+const blue2 = colorPalette(blue6, 2);
+const blue3 = colorPalette(blue6, 3);
+const blue4 = colorPalette(blue6, 4);
+const blue5 = colorPalette(blue6, 5);
+const blue6 = '#1890ff';
+const blue7 = colorPalette(blue6, 7);
+const blue8 = colorPalette(blue6, 8);
+const blue9 = colorPalette(blue6, 9);
+const blue10 = colorPalette(blue6, 10);
+
+const purple1 = colorPalette(purple6, 1);
+const purple2 = colorPalette(purple6, 2);
+const purple3 = colorPalette(purple6, 3);
+const purple4 = colorPalette(purple6, 4);
+const purple5 = colorPalette(purple6, 5);
+const purple6 = '#722ed1';
+const purple7 = colorPalette(purple6, 7);
+const purple8 = colorPalette(purple6, 8);
+const purple9 = colorPalette(purple6, 9);
+const purple10 = colorPalette(purple6, 10);
+
+const cyan1 = colorPalette(cyan6, 1);
+const cyan2 = colorPalette(cyan6, 2);
+const cyan3 = colorPalette(cyan6, 3);
+const cyan4 = colorPalette(cyan6, 4);
+const cyan5 = colorPalette(cyan6, 5);
+const cyan6 = '#13c2c2';
+const cyan7 = colorPalette(cyan6, 7);
+const cyan8 = colorPalette(cyan6, 8);
+const cyan9 = colorPalette(cyan6, 9);
+const cyan10 = colorPalette(cyan6, 10);
+
+const green1 = colorPalette(green6, 1);
+const green2 = colorPalette(green6, 2);
+const green3 = colorPalette(green6, 3);
+const green4 = colorPalette(green6, 4);
+const green5 = colorPalette(green6, 5);
+const green6 = '#52c41a';
+const green7 = colorPalette(green6, 7);
+const green8 = colorPalette(green6, 8);
+const green9 = colorPalette(green6, 9);
+const green10 = colorPalette(green6, 10);
+
+const magenta1 = colorPalette(magenta6, 1);
+const magenta2 = colorPalette(magenta6, 2);
+const magenta3 = colorPalette(magenta6, 3);
+const magenta4 = colorPalette(magenta6, 4);
+const magenta5 = colorPalette(magenta6, 5);
+const magenta6 = '#eb2f96';
+const magenta7 = colorPalette(magenta6, 7);
+const magenta8 = colorPalette(magenta6, 8);
+const magenta9 = colorPalette(magenta6, 9);
+const magenta10 = colorPalette(magenta6, 10);
+
+// alias of magenta
+const pink1 = colorPalette(pink6, 1);
+const pink2 = colorPalette(pink6, 2);
+const pink3 = colorPalette(pink6, 3);
+const pink4 = colorPalette(pink6, 4);
+const pink5 = colorPalette(pink6, 5);
+const pink6 = '#eb2f96';
+const pink7 = colorPalette(pink6, 7);
+const pink8 = colorPalette(pink6, 8);
+const pink9 = colorPalette(pink6, 9);
+const pink10 = colorPalette(pink6, 10);
+
+const red1 = colorPalette(red6, 1);
+const red2 = colorPalette(red6, 2);
+const red3 = colorPalette(red6, 3);
+const red4 = colorPalette(red6, 4);
+const red5 = colorPalette(red6, 5);
+const red6 = '#f5222d';
+const red7 = colorPalette(red6, 7);
+const red8 = colorPalette(red6, 8);
+const red9 = colorPalette(red6, 9);
+const red10 = colorPalette(red6, 10);
+
+const orange1 = colorPalette(orange6, 1);
+const orange2 = colorPalette(orange6, 2);
+const orange3 = colorPalette(orange6, 3);
+const orange4 = colorPalette(orange6, 4);
+const orange5 = colorPalette(orange6, 5);
+const orange6 = '#fa8c16';
+const orange7 = colorPalette(orange6, 7);
+const orange8 = colorPalette(orange6, 8);
+const orange9 = colorPalette(orange6, 9);
+const orange10 = colorPalette(orange6, 10);
+
+const yellow1 = colorPalette(yellow6, 1);
+const yellow2 = colorPalette(yellow6, 2);
+const yellow3 = colorPalette(yellow6, 3);
+const yellow4 = colorPalette(yellow6, 4);
+const yellow5 = colorPalette(yellow6, 5);
+const yellow6 = '#fadb14';
+const yellow7 = colorPalette(yellow6, 7);
+const yellow8 = colorPalette(yellow6, 8);
+const yellow9 = colorPalette(yellow6, 9);
+const yellow10 = colorPalette(yellow6, 10);
+
+const volcano1 = colorPalette(volcano6, 1);
+const volcano2 = colorPalette(volcano6, 2);
+const volcano3 = colorPalette(volcano6, 3);
+const volcano4 = colorPalette(volcano6, 4);
+const volcano5 = colorPalette(volcano6, 5);
+const volcano6 = '#fa541c';
+const volcano7 = colorPalette(volcano6, 7);
+const volcano8 = colorPalette(volcano6, 8);
+const volcano9 = colorPalette(volcano6, 9);
+const volcano10 = colorPalette(volcano6, 10);
+
+const geekblue1 = colorPalette(geekblue6, 1);
+const geekblue2 = colorPalette(geekblue6, 2);
+const geekblue3 = colorPalette(geekblue6, 3);
+const geekblue4 = colorPalette(geekblue6, 4);
+const geekblue5 = colorPalette(geekblue6, 5);
+const geekblue6 = '#2f54eb';
+const geekblue7 = colorPalette(geekblue6, 7);
+const geekblue8 = colorPalette(geekblue6, 8);
+const geekblue9 = colorPalette(geekblue6, 9);
+const geekblue10 = colorPalette(geekblue6, 10);
+
+const lime1 = colorPalette(lime6, 1);
+const lime2 = colorPalette(lime6, 2);
+const lime3 = colorPalette(lime6, 3);
+const lime4 = colorPalette(lime6, 4);
+const lime5 = colorPalette(lime6, 5);
+const lime6 = '#a0d911';
+const lime7 = colorPalette(lime6, 7);
+const lime8 = colorPalette(lime6, 8);
+const lime9 = colorPalette(lime6, 9);
+const lime10 = colorPalette(lime6, 10);
+
+const gold1 = colorPalette(gold6, 1);
+const gold2 = colorPalette(gold6, 2);
+const gold3 = colorPalette(gold6, 3);
+const gold4 = colorPalette(gold6, 4);
+const gold5 = colorPalette(gold6, 5);
+const gold6 = '#faad14';
+const gold7 = colorPalette(gold6, 7);
+const gold8 = colorPalette(gold6, 8);
+const gold9 = colorPalette(gold6, 9);
+const gold10 = colorPalette(gold6, 10);
+
+const presetcolors = [
+  'pink',
+  'magenta',
+  'red',
+  'volcano',
+  'orange',
+  'yellow',
+  'gold',
+  'cyan',
+  'lime',
+  'green',
+  'blue',
+  'geekblue',
+  'purple',
+];
