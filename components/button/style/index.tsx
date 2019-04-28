05fce4ede56f17bd7b8e761aa681a9d425f583ee
Brandon Kalinowski <brandon@brandonkalinowski.com>
2019-04-28 02:05:11 -0700

    button mixin

--- components/button/style/index.tsx
+++ components/button/style/index.tsx
@@ -1,2 +1,2 @@
-import '../../style/index.less';
+import '../../style/index.less.js';
 import './index.less';
--- components/button/style/mixin.js
+++ components/button/style/mixin.js
@@ -1,197 +1,347 @@
-import '../../style/themes/default';
-import '../../style/mixins/index';
-import './mixin';
-import styled from 'styled-components';
+// @ts-nocheck
+import { css } from 'linaria'
 
-const btnPrefixCls = 'ant-prefix-btn';
+// mixins for button
+// ------------------------
+export function buttonSize(height, padding, fontSize, borderRadius) {
+  return css`
+  height: ${height};
+  padding: ${padding};
+  font-size: ${fontSize};
+  border-radius: ${borderRadius};
+`
+}
 
-// for compatible
-const btnGhostColor = textColor;
-const btnGhostBg = transparent;
-const btnGhostBorder = borderColorBase;
-
-
-// Button styles
-// -----------------------------
-const Button = styled.button`
-  /* Fixing https://github.com/ant-design/ant-design/issues/12978
-  It is a render problem of chrome, which is only happened in the codesandbox demo
-  0.001px solution works and I don't why */
-  line-height: line-height-base - 0.001;
-  .btn;
-  .btn-default;
-
-  /* Make sure that the target of Button's click event always be 'button'
-    Ref: https://github.com/ant-design/ant-design/issues/7034 */
-  & i,
-  & span {
-    display: inline-block;
-    pointer-events: none;
-  }
-
-  &-primary {
-    .btn-primary;
-
-    .${btnPrefixCls}-group &:not(:first-child):not(:last-child) {
-      border-right-color: btn-group-border;
-      border-left-color: btn-group-border;
-
-      &:disabled {
-        border-color: btn-default-border;
-      }
-    }
-
-    .${btnPrefixCls}-group &:first-child {
-      &:not(:last-child) {
-        border-right-color: btn-group-border;
-        &[disabled] {
-          border-right-color: btn-default-border;
-        }
-      }
-    }
-
-    .${btnPrefixCls}-group &:last-child:not(:first-child),
-    .${btnPrefixCls}-group & + & {
-      border-left-color: btn-group-border;
-      &[disabled] {
-        border-left-color: btn-default-border;
-      }
+export const buttonDisabled = css`
+  &Disabled,
+  &.disabled,
+  &[disabled] {
+    &,
+    &:hover,
+    &:focus,
+    &:active,
+    &.active {
+      ${buttonColor(btnDisableColor, btnDisableBg, btnDisableBorder)}
+      text-shadow: none;
+      box-shadow: none;
     }
   }
+`
 
-  &-ghost {
-    .btn-ghost;
+function buttonVariantPrimary(color, background) {
+  return css`
+  ${buttonColor(color, background, background)};
+
+  text-shadow: btnTextShadow;
+  box-shadow: btnPrimaryShadow;
+
+  &:hover,
+  &:focus {
+    .buttonColor(
+      color, colorPalette('@{background}', 5) `, colorPalette('@{background}', 5) `
+    );
   }
 
-  &-dashed {
-    .btn-dashed;
+  &:active,
+  &.active {
+    .buttonColor(
+      color, colorPalette('@{background}', 7) `, colorPalette('@{background}', 7) `
+    );
   }
 
-  &-danger {
-    .btn-danger;
+  .buttonDisabled();
+`
+}
+
+.buttonVariantOther(color; background; border) {
+  ${buttonColor(color; background; border)};
+
+  &:hover,
+  &:focus {
+    .buttonColor(
+      colorPalette('@{btnPrimaryBg}', 5), background, colorPalette('@{btnPrimaryBg}', 5)
+        `
+    );
   }
-
-  &-round {
-    .btn-round(btn-prefix-cls);
+  &:active,
+  &.active {
+    .buttonColor(
+      colorPalette('@{btnPrimaryBg}', 7), background, colorPalette('@{btnPrimaryBg}', 7)
+        `
+    );
   }
-
-  &-circle,
-  &-circle-outline {
-    .btn-circle(btn-prefix-cls);
+  .buttonDisabled();
+}
+.buttonVariantDanger(color; background; border) {
+  ${buttonColor(color; background; border)};
+  &:hover {
+    .buttonColor(btnPrimaryColor, colorPalette(color, 5), colorPalette(color, 5));
   }
-
-  &::before {
-    position: absolute;
-    top: -1px;
-    right: -1px;
-    bottom: -1px;
-    left: -1px;
-    z-index: 1;
-    display: none;
-    background: component-background;
-    border-radius: inherit;
-    opacity: 0.35;
-    transition: opacity 0.2s;
-    content: '';
-    pointer-events: none;
+  &:focus {
+    .buttonColor(colorPalette(color, 5), componentBackground, colorPalette(color, 5));
   }
+  &:active,
+  &.active {
+    ${buttonColor(
+      btnPrimaryColor, colorPalette(color, 7), colorPalette(color, 7)
+    )};
+  }
+  .buttonDisabled();
+}
 
-  .{iconfont-css-prefix} {
-    transition: margin-left 0.3s ease-in-out;
+function buttonVariantGhost(color) {
+  return css`
+  ${buttonColor(color, transparent, color)};
 
-    // Follow icon blur under windows. Change the render.
-    // https://github.com/ant-design/ant-design/issues/13924
-    &.{iconfont-css-prefix}-plus,
-    &.{iconfont-css-prefix}-minus {
-      > svg {
-        shape-rendering: optimizeSpeed;
-      }
+  text-shadow: none;
+  &:hover,
+  &:focus {
+    ${buttonColor(colorPalette(color, 5), transparent, colorPalette(color, 5))};
+  }
+  &:active,
+  &.active {
+    ${buttonColor(colorPalette(color, 7), transparent, colorPalette(color, 7))};
+  }
+  ${buttonDisabled()};
+`
+}
+
+// buttonColor defintion
+function buttonColor(color, background, border) {
+  return css`
+  color: ${color};
+  background-color: ${background};
+  border-color: ${border};
+  /* // a inside Button which only work in Chrome */
+  /* http://stackoverflow.com/a/17253457 */
+  & > a:only-child {
+    color: currentColor;
+    &::after {
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      background: transparent;
+      content: '';
     }
   }
+  `
+}
 
-  &&-loading {
+
+.buttonGroupBase(btnClassName) {
+  position: relative;
+  display: inlineBlock;
+  > .@{btnClassName},
+  > span > .@{btnClassName} {
     position: relative;
-    pointer-events: none;
-  }
-
-  &&-loading::before {
-    display: block;
-  }
-
-  &&-loading:not(&-circle):not(&-circle-outline):not(&-icon-only) {
-    padding-left: 29px;
-    .{iconfont-css-prefix}:not(:last-child) {
-      margin-left: -14px;
+    &:hover,
+    &:focus,
+    &:active,
+    &.active {
+      zIndex: 2;
+    }
+    &:disabled {
+      zIndex: 0;
     }
   }
+  // size
+  &Lg > .@{btnClassName},
+  &Lg > span > .@{btnClassName} {
+    .buttonSize(btnHeightLg; btnPaddingLg; btnFontSizeLg; 0);
 
-  &-sm&-loading:not(&-circle):not(&-circle-outline):not(&-icon-only) {
-    padding-left: 24px;
-    .{iconfont-css-prefix} {
-      margin-left: -17px;
+    lineHeight: btnHeightLg - 2px;
+  }
+  &Sm > .@{btnClassName},
+  &Sm > span > .@{btnClassName} {
+    .buttonSize(btnHeightSm; btnPaddingSm; fontSizeBase; 0);
+
+    lineHeight: btnHeightSm - 2px;
+    > .@{iconfontCssPrefix} {
+      fontSize: fontSizeBase;
     }
   }
+}
+// Base styles of buttons
+// TODO: check which other components are using this .btn; mixin
+// --------------------------------------------------
+const btn = css`
+  position: relative;
+  display: inlineBlock;
+  font-weight: btnFontWeight;
+  white-space: nowrap;
+  text-align: center;
+  background-image: none;
+  border: btnBorderWidth btnBorderStyle transparent;
+  box-Shadow: ${btnShadow};
+  cursor: pointer;
+  transition: all 0.3s easeInOut;
+  user-select: none;
+  touch-action: manipulation;
+  ${buttonSize(btnHeightBase, btnPaddingBase, fontSizeBase, btnBorderRadiusBase)};
 
-  &-group {
-    .btn-group(btn-prefix-cls);
+  > .${iconfontCssPrefix} {
+    line-Height: 1;
   }
-
-  &:not(&-circle):not(&-circle-outline)&-icon-only {
-    padding-right: 8px;
-    padding-left: 8px;
+  &,
+  &:active,
+  &:focus {
+    outline: 0;
   }
-
-  // http://stackoverflow.com/a/21281554/3040605
-  &:focus > span,
-  &:active > span {
-    position: relative;
+  &:not([disabled]):hover {
+    text-Decoration: none;
   }
-
-  // To ensure that a space will be placed between character and `Icon`.
-  > .{iconfont-css-prefix} + span,
-  > span + .{iconfont-css-prefix} {
-    margin-left: 8px;
+  &:not([disabled]):active {
+    outline: 0;
+    box-Shadow: none;
   }
-
-  &-background-ghost {
-    color: component-background;
-    background: transparent !important;
-    border-color: component-background;
+  &.disabled,
+  &[disabled] {
+    cursor: notAllowed;
+    > * {
+      pointer-Events: none;
+    }
   }
-
-  &-background-ghost&-primary {
-    .button-variant-ghost(btn-primary-bg);
+  &Lg {
+    ${buttonSize(btnHeightLg, btnPaddingLg, btnFontSizeLg, btnBorderRadiusBase)};
   }
-
-  &-background-ghost&-danger {
-    .button-variant-ghost(btn-danger-color);
+  &Sm {
+    ${buttonSize(btnHeightSm, btnPaddingSm, btnFontSizeSm, btnBorderRadiusSm)};
   }
+`
 
-  &-two-chinese-chars::first-letter {
-    letter-spacing: 0.34em;
+
+// primary button style
+.btnPrimary() {
+  .buttonVariantPrimary(btnPrimaryColor; btnPrimaryBg);
+}
+// default button style
+.btnDefault() {
+  .buttonVariantOther(btnDefaultColor; btnDefaultBg; btnDefaultBorder);
+  &:hover,
+  &:focus,
+  &:active,
+  &.active {
+    textDecoration: none;
+    background: btnDefaultBg;
   }
+}
+// ghost button style
+.btnGhost() {
+  .buttonVariantOther(btnGhostColor, btnGhostBg, btnGhostBorder);
+}
+// dashed button style
+.btnDashed() {
+  .buttonVariantOther(btnDefaultColor, btnDefaultBg, btnDefaultBorder);
 
-  &-two-chinese-chars > *:not(.{iconfont-css-prefix}) {
-    margin-right: -0.34em;
-    letter-spacing: 0.34em;
+  borderStyle: dashed;
+}
+// danger button style
+.btnDanger() {
+  .buttonVariantDanger(btnDangerColor, btnDangerBg, btnDangerBorder);
+}
+// round button
+.btnRound(btnClassName: btn) {
+  .buttonSize(btnCircleSize; 0 btnCircleSize / 2; fontSizeBase + 2px; btnCircleSize);
+  &.@{btnClassName}Lg {
+    .buttonSize(
+      btnCircleSizeLg; 0 btnCircleSizeLg / 2; btnFontSizeLg + 2px; btnCircleSizeLg
+    );
   }
-
-  &-block {
-    width: 100%;
+  &.@{btnClassName}Sm {
+    .buttonSize(
+      btnCircleSizeSm; 0 btnCircleSizeSm / 2; fontSizeBase; btnCircleSizeSm
+    );
   }
-
-  // https://github.com/ant-design/ant-design/issues/12681
-  &:empty {
-    vertical-align: top;
+}
+// circle button: the content only contains icon
+.btnCircle(btnClassName: btn) {
+  .square(btnCircleSize);
+  .buttonSize(btnCircleSize; 0; fontSizeBase + 2px; 50%);
+  &.@{btnClassName}Lg {
+    .square(btnCircleSizeLg);
+    .buttonSize(btnCircleSizeLg; 0; btnFontSizeLg + 2px; 50%);
+  }
+  &.@{btnClassName}Sm {
+    .square(btnCircleSizeSm);
+    .buttonSize(btnCircleSizeSm; 0; fontSizeBase; 50%);
+  }
+}
+// Horizontal button groups styl
+// --------------------------------------------------
+.btnGroup(btnClassName: btn) {
+  .buttonGroupBase(btnClassName);
+  .@{btnClassName} + .@{btnClassName},
+  .@{btnClassName} + &,
+  span + .@{btnClassName},
+  .@{btnClassName} + span,
+  > span + span,
+  & + .@{btnClassName},
+  & + & {
+    marginLeft: -1px;
+  }
+  .@{btnClassName}Primary + .@{btnClassName}:not(.@{btnClassName}Primary):not([disabled]) {
+    borderLeftColor: transparent;
+  }
+  .@{btnClassName} {
+    borderRadius: 0;
+  }
+  > .@{btnClassName}:firstChild,
+  > span:firstChild > .@{btnClassName} {
+    marginLeft: 0;
+  }
+  > .@{btnClassName}:onlyChild {
+    borderRadius: btnBorderRadiusBase;
+  }
+  > span:onlyChild > .@{btnClassName} {
+    borderRadius: btnBorderRadiusBase;
+  }
+  > .@{btnClassName}:firstChild:not(:lastChild),
+  > span:firstChild:not(:lastChild) > .@{btnClassName} {
+    borderTopLeftRadius: btnBorderRadiusBase;
+    borderBottomLeftRadius: btnBorderRadiusBase;
+  }
+  > .@{btnClassName}:lastChild:not(:firstChild),
+  > span:lastChild:not(:firstChild) > .@{btnClassName} {
+    borderTopRightRadius: btnBorderRadiusBase;
+    borderBottomRightRadius: btnBorderRadiusBase;
+  }
+  &Sm {
+    > .@{btnClassName}:onlyChild {
+      borderRadius: btnBorderRadiusSm;
+    }
+    > span:onlyChild > .@{btnClassName} {
+      borderRadius: btnBorderRadiusSm;
+    }
+    > .@{btnClassName}:firstChild:not(:lastChild),
+    > span:firstChild:not(:lastChild) > .@{btnClassName} {
+      borderTopLeftRadius: btnBorderRadiusSm;
+      borderBottomLeftRadius: btnBorderRadiusSm;
+    }
+    > .@{btnClassName}:lastChild:not(:firstChild),
+    > span:lastChild:not(:firstChild) > .@{btnClassName} {
+      borderTopRightRadius: btnBorderRadiusSm;
+      borderBottomRightRadius: btnBorderRadiusSm;
+    }
+  }
+  & > & {
+    float: left;
+  }
+  & > &:not(:firstChild):not(:lastChild) > .@{btnClassName} {
+    borderRadius: 0;
+  }
+  & > &:firstChild:not(:lastChild) {
+    > .@{btnClassName}:lastChild {
+      paddingRight: 8px;
+      borderTopRightRadius: 0;
+      borderBottomRightRadius: 0;
+    }
+  }
+  & > &:lastChild:not(:firstChild) > .@{btnClassName}:firstChild {
+    paddingLeft: 8px;
+    borderTopLeftRadius: 0;
+    borderBottomLeftRadius: 0;
   }
 }
 
-a.{btn-prefix-cls} {
-  line-height: btn-height-base - 2px;
-  &-lg {
-    line-height: btn-height-lg - 2px;
-  }
-  &-sm {
-    line-height: btn-height-sm - 2px;
-  }
-`
