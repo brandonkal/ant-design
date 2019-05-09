ded61b10a7bee84cd45e73fa4bbc354ca77543a4
Brandon Kalinowski <brandon@brandonkalinowski.com>
2019-05-09 16:08:46 -0700

    remove old files

--- components/_util/reactNode.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export function cloneElement(element: React.ReactNode, ...restArgs: any[]) {
-  if (!React.isValidElement(element)) return element;
-
-  return React.cloneElement(element, ...restArgs);
-}
