bc83338f2c084e5ece5b2f7d7a7cc88548e46b14
Brandon Kalinowski <brandon@brandonkalinowski.com>
2019-05-09 16:28:22 -0700

    Remove tests and CN docs

---
+++ gulpfile.js
@@ -0,0 +1,5 @@
+//  Gulp File to process compiled CSS files to remove autoprefix convert to emotion, and Save with original components
+function defaultTask(cb) {
+  cb();
+}
+exports.default = defaultTask;
